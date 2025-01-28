ce15f5e7e94d96bfad6d8423dad1ca4c0b6f8781
chr1scr0sss <tiwakorn.matonboon@gmail.com>
2025-01-28 22:48:55 +0700

    lab15_4.cpp completed

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -19,3 +19,23 @@ int main(){
 }
 
 //Do not modify source code above this line
+
+void shuffle(int &a, int &b, int &c, int &d){
+    int satangs[4] = {a, b, c, d};
+    bool used[4] = {false, false, false, false};
+    
+    int salaped[4];
+    for(int i =0; i<4; i++){
+        int index;
+        do{
+            index = rand()%4;
+        }while (used[index]);
+        salaped[i] = satangs[index];
+        used[index] = true;
+    }
+    a = salaped[0];
+    b = salaped[1];
+    c = salaped[2];
+    d = salaped[3];
+    
+}
